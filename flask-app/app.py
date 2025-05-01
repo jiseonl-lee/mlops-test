fc625c8b1138f77d6a7023fabf0b1cf6994177bb
leanna.j <leanna.j@kakaomobility.com>
2025-05-01 23:33:25 +0900

    deploy flask-app

---
+++ flask-app/app.py
@@ -0,0 +1,9 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return jsonify({'message': 'Hello from Azure Flask App!'})
+
+if __name__ == '__main__':
+    app.run()
